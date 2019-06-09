ef4c427b54c4d1b04908298849f5d2cb1ebae9c8
Lane Spangler <lspangler@protonmail.com>
2019-06-08 21:48:46 -0500

    Parse post body before storing, accept preview option
    The preview option will emit some kind of server-side event in the future. A
    frontend will listen for the event and render the post in order to provide an
    instant preview to the user.

--- src/org_blog/handler.clj
+++ src/org_blog/handler.clj
@@ -5,15 +5,25 @@
              [wrap-defaults site-defaults api-defaults]]
             [org-blog.db :refer [db]]
             [org-blog.db.posts :as posts]
-            [org-blog.helper.routes :refer [defroutes-api]]))
+            [org-blog.helper.routes :refer [defroutes-api]]
+            [org-parser.core :as org-parser]
+            [cheshire.core :as json]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
+(defn add-or-preview-post [body]
+  (let [{:keys [filename post preview]} body
+        post-json (-> post org-parser/parse json/generate-string)]
+    (if (#{"true"} preview)
+      (println "This should send a preview event")
+      (posts/insert db {:filename filename
+                        :post post-json}))))
+
 (defroutes-api api-routes
   (GET "posts" [] (posts/get-all db))
-  (POST "post" body (posts/insert db body))
+  (POST "post" body (add-or-preview-post body))
   (GET "post/:filename"
     [filename]
     (posts/get-by-filename db {:filename filename})))
