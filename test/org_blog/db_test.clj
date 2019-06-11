339c53bf00289ee0c82c26c11fb5bf0a7aeada87
Lane Spangler <lspangler@protonmail.com>
2019-06-10 22:49:39 -0500

    Change test db connection string

--- test/org_blog/db_test.clj
+++ test/org_blog/db_test.clj
@@ -2,7 +2,7 @@
   (:require [migratus.core :as migratus]
             [org-blog.db :refer :all]))
 
-(def test-db (assoc db :subname "//0.0.0.0/org_blog_test"))
+(def test-db (assoc db :subname "//localhost/org_blog_test"))
 (def config (assoc migratus-config :db test-db))
 
 (defn db-fixture [f]
