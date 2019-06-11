24fd4150f8eb72898724d81c29238b3805022fec
Lane Spangler <lspangler@protonmail.com>
2019-06-11 14:55:29 -0500

    Add repo url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org-blog "0.1.0-SNAPSHOT"
   :description "Backend for storing and parsing .org blog posts"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/lane-s/org-blog"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :min-lein-version "2.0.0"
